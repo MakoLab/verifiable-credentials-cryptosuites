50bc7ca8fe64f09f2bbd9a8b9a323d36b7bc397d
Dominik.Kuzinski <dominik.kuzinski@makolab.com>
2024-01-31 16:05:25 +0100

    Added RDFC test cases

---
+++ RDFDatasetCanonicalization.Tests/CanonicalizationTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsonLdExtensions.Canonicalization;
+using VDS.RDF;
+
+namespace RDFDatasetCanonicalization.Tests
+{
+    public class CanonicalizationTests
+    {
+        [Theory]
+        [ClassData(typeof(RDFCTestData))]
+        public void TestPositiveCases(string name, string input, string expected)
+        {
+            var dataset = new TripleStore();
+            dataset.LoadFromString(input);
+            var actual = dataset.Normalize();
+            Assert.Equal(expected, actual.Serialize());
+        }
+    }
+}
---
+++ RDFDatasetCanonicalization.Tests/RDFCTestData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace RDFDatasetCanonicalization.Tests
+{
+    public class RDFCTestData : TheoryData<string, string, string>
+    {
+        private List<string> _testCases = new();
+        private List<string> _expectedResults = new();
+
+        public RDFCTestData()
+        {
+            // Read manifest.jsonld
+            var manifest = JObject.Parse(System.IO.File.ReadAllText("manifest.jsonld"));
+            var entries = manifest["entries"];
+            foreach (var entry in entries!)
+            {
+                var name = entry["name"]?.ToString();
+                var isPositive = entry.Value<string>("type") != "rdfc:RDFC10NegativeEvalTest";
+                if (isPositive)
+                {
+                    var input = Path.Combine("TestCases", entry["action"]?.ToString());
+                    var expected = Path.Combine("Results", entry["result"]?.ToString());
+                    Add(name, File.ReadAllText(input), File.ReadAllText(expected));
+                }
+            }
+        }
+    }
+}
