cc583e06d5c696ad56d53c519bbf35bedac48fde
Dominik Kuziński <dominik.kuzinski@makolab.com>
2024-04-18 19:06:50 +0200

    Added new cryptosuite - "ecdsa-sd-2023"

---
+++ ECDsa-sd-2023-Cryptosuite/ECDsaSd2023Cryptosuite.cs
@@ -0,0 +1,22 @@
+﻿using Cryptosuite.Core;
+using Cryptosuite.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECDsa_sd_2023_Cryptosuite
+{
+    public class ECDsaSd2023Cryptosuite : ICryptosuite
+    {
+        public string RequiredAlgorithm { get => "P-256"; }
+
+        public string Name { get => "ecdsa-sd-2023"; }
+
+        public Verifier CreateVerifier(VerificationMethod verificationMethod)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
