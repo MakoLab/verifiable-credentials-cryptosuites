9f84d959e44b3647fb5f13d36ae47eeedcb5cb6a
Dominik Kuziński <dominik.kuzinski@makolab.com>
2024-10-23 18:05:41 +0200

    Changed methods to expression bodies.

--- Cryptosuite/Constants.cs
+++ Cryptosuite/Constants.cs
@@ -19,70 +19,46 @@ namespace Cryptosuite.Core
         public const string P384 = "P-384";
         public const string P521 = "P-521";
 
-        public static X9ECParameters ToECCurve(string curve)
-        {
-            return ECNamedCurveTable.GetByName(curve);
-        }
+        public static X9ECParameters ToECCurve(string curve) => ECNamedCurveTable.GetByName(curve);
 
-        public static DerObjectIdentifier ToDerObjectIdentifier(this ECDsaCurveType curve)
+        public static DerObjectIdentifier ToDerObjectIdentifier(this ECDsaCurveType curve) => curve switch
         {
-            return curve switch
-            {
-                ECDsaCurveType.P256 => BsiObjectIdentifiers.ecdsa_plain_SHA256,
-                ECDsaCurveType.P384 => BsiObjectIdentifiers.ecdsa_plain_SHA384,
-                ECDsaCurveType.P521 => BsiObjectIdentifiers.ecdsa_plain_SHA512,
-                _ => throw new NotSupportedException($"Curve {curve} is not supported."),
-            };
-        }
+            ECDsaCurveType.P256 => BsiObjectIdentifiers.ecdsa_plain_SHA256,
+            ECDsaCurveType.P384 => BsiObjectIdentifiers.ecdsa_plain_SHA384,
+            ECDsaCurveType.P521 => BsiObjectIdentifiers.ecdsa_plain_SHA512,
+            _ => throw new NotSupportedException($"Curve {curve} is not supported."),
+        };
 
-        public static string ToAlgorithmName(this DerObjectIdentifier id)
+        public static string ToAlgorithmName(this DerObjectIdentifier id) => id switch
         {
-            if (id.Id == BsiObjectIdentifiers.ecdsa_plain_SHA256.Id)
-            {
-                return P256;
-            }
-            if (id.Id == BsiObjectIdentifiers.ecdsa_plain_SHA384.Id)
-            {
-                return P384;
-            }
-            if (id.Id == BsiObjectIdentifiers.ecdsa_plain_SHA512.Id)
-            {
-                return P521;
-            }
-            throw new NotSupportedException($"Curve {id.Id} is not supported.");
-        }
+            { Id: var der } when der == BsiObjectIdentifiers.ecdsa_plain_SHA256.Id => P256,
+            { Id: var der } when der == BsiObjectIdentifiers.ecdsa_plain_SHA384.Id => P384,
+            { Id: var der } when der == BsiObjectIdentifiers.ecdsa_plain_SHA512.Id => P521,
+            _ => throw new NotSupportedException($"Curve {id.Id} is not supported."),
+        };
 
-        public static ECDsaCurveType ToECDsaCurveType(string curve)
+        public static ECDsaCurveType ToECDsaCurveType(string curve) => curve switch
         {
-            return curve switch
-            {
-                P256 => ECDsaCurveType.P256,
-                P384 => ECDsaCurveType.P384,
-                P521 => ECDsaCurveType.P521,
-                _ => throw new NotSupportedException($"Curve {curve} is not supported."),
-            };
-        }
+            P256 => ECDsaCurveType.P256,
+            P384 => ECDsaCurveType.P384,
+            P521 => ECDsaCurveType.P521,
+            _ => throw new NotSupportedException($"Curve {curve} is not supported."),
+        };
 
-        public static IDigest GetDigest(this ECDsaCurveType curve)
+        public static IDigest GetDigest(this ECDsaCurveType curve) => curve switch
         {
-            return curve switch
-            {
-                ECDsaCurveType.P256 => new Sha256Digest(),
-                ECDsaCurveType.P384 => new Sha384Digest(),
-                ECDsaCurveType.P521 => new Sha512Digest(),
-                _ => throw new NotSupportedException($"Curve {curve} is not supported."),
-            };
-        }
+            ECDsaCurveType.P256 => new Sha256Digest(),
+            ECDsaCurveType.P384 => new Sha384Digest(),
+            ECDsaCurveType.P521 => new Sha512Digest(),
+            _ => throw new NotSupportedException($"Curve {curve} is not supported."),
+        };
 
-        public static string ToString(this ECDsaCurveType curve)
+        public static string ToString(this ECDsaCurveType curve) => curve switch
         {
-            return curve switch
-            {
-                ECDsaCurveType.P256 => P256,
-                ECDsaCurveType.P384 => P384,
-                ECDsaCurveType.P521 => P521,
-                _ => throw new NotSupportedException($"Curve {curve} is not supported."),
-            };
-        }
+            ECDsaCurveType.P256 => P256,
+            ECDsaCurveType.P384 => P384,
+            ECDsaCurveType.P521 => P521,
+            _ => throw new NotSupportedException($"Curve {curve} is not supported."),
+        };
     }
 }
