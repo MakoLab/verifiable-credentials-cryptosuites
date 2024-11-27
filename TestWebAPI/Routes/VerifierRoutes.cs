5d1c2034bbe7685551d4c43211e64ab6bf4f14aa
Dominik Kuziński <dominik.kuzinski@makolab.com>
2024-11-27 15:33:45 +0100

    Refactored the VerifyCredential method in VerifierHandlers to improve readability and maintainability by adding a new parameter, simplifying the method body, and introducing a new private static method, ProcessVerificationRequest.

--- TestWebAPI/Routes/VerifierRoutes.cs
+++ TestWebAPI/Routes/VerifierRoutes.cs
@@ -1,4 +1,5 @@
-﻿using DataIntegrity;
+﻿using Cryptosuite.Core.Interfaces;
+using DataIntegrity;
 using ECDsa_2019_Cryptosuite;
 using ECDsa_Multikey;
 using JsonLdSignatures;
@@ -23,56 +24,22 @@ namespace TestWebAPI.Routes
 
     public class VerifierHandlers
     {
-        public IResult VerifyCredential([FromBody] object json, ILogger<VerifierHandlers> logger, MockDataProvider mdp, ICryptosuiteResolver resolver)
+        public IResult VerifyCredential([FromBody] object json,
+                                        ILogger<VerifierHandlers> logger,
+                                        ICryptosuiteResolver resolver,
+                                        IDidDocumentCreator didDocumentCreator)
         {
             var jsonStr = JsonSerializer.Serialize(json);
             logger.LogDebug("Verifier Request:\n=================");
             logger.LogDebug("{Request}", jsonStr);
-            if (JObject.Parse(jsonStr)["verifiableCredential"] is not JObject jsonObj)
-            {
-                return Results.BadRequest("Invalid JSON");
-            }
-            var cryptosuiteName = GetCryptosuiteName(jsonObj);
-            if (cryptosuiteName is null)
-            {
-                return Results.BadRequest("Cryptosuite not found in request document.");
-            }
-            cryptosuiteName = $"{cryptosuiteName}-verify";
-            var cryptosuite = resolver.GetCryptosuite(cryptosuiteName);
-            if (cryptosuite is null)
-            {
-                return Results.BadRequest("Cryptosuite not recognized.");
-            }
-            var verificationMethodId = GetVerificationMethodId(jsonObj);
-            if (verificationMethodId is null)
-            {
-                return Results.BadRequest("Verification method not found in request document.");
-            }
-            Uri vmUri;
             try
             {
-                vmUri = new Uri(verificationMethodId);
-            }
-            catch (Exception)
-            {
-                return Results.BadRequest("Verification method is not a valid URI.");
-            }
-            var documentLoader = new VCDIDocumentLoader();
-            if (documentLoader.LoadDocument(vmUri)?.Document is not JObject verificationMethodObject)
-            {
-                return Results.BadRequest("Unable to load Verification Method document.");
-            }
-            var mvm = verificationMethodObject.ToObject<MultikeyVerificationMethod>();
-            if (mvm is null)
-            {
-                return Results.BadRequest("Verification Method document is not a valid MultikeyVerificationMethod.");
-            }
-            var keypair = MultikeyService.From(mvm);
-            var suite = new DataIntegrityProof(cryptosuite, keypair.Signer);
-            var jss = new JsonLdSignatureService();
-            try
-            {
-                var result = jss.Verify(jsonObj, suite, new AssertionMethodPurpose(), documentLoader);
+                var (document, cryptosuiteName, keypair) = ProcessVerificationRequest(didDocumentCreator, jsonStr);
+                var cryptosuite = resolver.GetCryptosuite(cryptosuiteName) ?? throw new ArgumentException("Cryptosuite not found.");
+                var suite = new DataIntegrityProof(cryptosuite, keypair.Signer);
+                var jss = new JsonLdSignatureService();
+                var documentLoader = new VCDIDocumentLoader(didDocumentCreator);
+                var result = jss.Verify(document, suite, new AssertionMethodPurpose(), documentLoader);
                 var response = JsonLdSignatureService.ToJsonResult(result).ToString();
                 logger.LogDebug("Verifier response:\n==================");
                 logger.LogDebug("{Response}", response);
@@ -87,6 +54,27 @@ namespace TestWebAPI.Routes
                 logger.LogError("{Response}", response);
                 return Results.BadRequest(response);
             }
+        }
+
+        private static (JObject, string, KeyPairInterface) ProcessVerificationRequest(IDidDocumentCreator didDocumentCreator, string jsonStr)
+        {
+            if (JObject.Parse(jsonStr)["verifiableCredential"] is not JObject jsonObj)
+            {
+                throw new ArgumentException("Invalid JSON");
+            }
+            var cryptosuiteName = GetCryptosuiteName(jsonObj) ?? throw new ArgumentException("Cryptosuite not found in request document.");
+            cryptosuiteName = $"{cryptosuiteName}-verify";
+            var verificationMethodId = GetVerificationMethodId(jsonObj) ?? throw new ArgumentException("Verification method not found in request document.");
+            Uri vmUri;
+            vmUri = new Uri(verificationMethodId);
+            var documentLoader = new VCDIDocumentLoader(didDocumentCreator);
+            if (documentLoader.LoadDocument(vmUri)?.Document is not JObject verificationMethodObject)
+            {
+                throw new ArgumentException("Unable to load Verification Method document.");
+            }
+            var mvm = verificationMethodObject.ToObject<MultikeyVerificationMethod>() ?? throw new ArgumentException("Verification Method document is not a valid MultikeyVerificationMethod.");
+            var keypair = MultikeyService.From(mvm);
+            return (jsonObj, cryptosuiteName, keypair);
         }
 
         private static string? GetCryptosuiteName(JObject jObject)
